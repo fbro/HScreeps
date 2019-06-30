53fd94bc309709736db8bd8847b4a7a195e0e76e
Frederik <hulmir@gmail.com>
2019-06-30 23:06:58 +0200

    memrooms update

--- v2/MemRooms.js
+++ v2/MemRooms.js
@@ -19,7 +19,7 @@ const MemRooms = {
             for (let gameRoomKey in Game.rooms) {
                 const gameRoom = Game.rooms[gameRoomKey]; // visible room
                 if (gameRoom.controller) { // has a controller - is ownable
-                    let updateMemRoom = true;
+                    let fullUpdate = true;
                     let oldMemRoom = [];
                     for (let memRoomKey in Memory.MemRooms) {
                         const memRoom = Memory.MemRooms[memRoomKey]; // memory room
@@ -27,26 +27,26 @@ const MemRooms = {
                             oldMemRoom = memRoom;
                             if(gameRoom.controller.my){ // still my room
                                 if(gameRoom.controller.level !== memRoom.RoomLevel){ // room found and room has changed level - also update the room
-                                    updateMemRoom = true;
-                                }else{ // room found and it is my room and it has not changed level - do not update
-                                    updateMemRoom = false;
+                                    fullUpdate = true;
+                                }else{ // room found and it is my room and it has not changed level - do not update just refresh
+                                    fullUpdate = false;
                                 }
                             }else{ // not my room anymore
-                                console.log("MemRooms, UpdateMemRooms: do no own " + gameRoom.name + " anymore. removing room from mem");
+                                console.log("MemRooms, UpdateMemRooms: do not own " + gameRoom.name + " anymore. removing room from mem");
                                 Memory.MemRooms[gameRoom.name] = undefined; // remove room - I do no own it anymore
-                                updateMemRoom = false;
+                                fullUpdate = false;
                             }
                             break;
                         }
                     }
-                    if(updateMemRoom && gameRoom.controller.my){
-                        CreateDefaultJobs(gameRoom, oldMemRoom.RoomObjJobs);
+                    if(gameRoom.controller.my){
+                        UpdateJobsInRoom(gameRoom, oldMemRoom.RoomObjJobs, fullUpdate);
                     }
                 }
             }
         }
 
-        function CreateDefaultJobs(gameRoom, oldRoomObjJobs){
+        function UpdateJobsInRoom(gameRoom, oldRoomObjJobs, fullUpdate){
             let roomObjJobs = [];
             switch (gameRoom.controller.level) {
                 case 8:
@@ -73,23 +73,25 @@ const MemRooms = {
                     RefreshFillTowerJobs(gameRoom, roomObjJobs);
                 case 2:
                 case 1:
-                    // Controller
-                    new RoomVisual(gameRoom.name).text("💼", gameRoom.controller.pos.x, gameRoom.controller.pos.y);
-                    roomObjJobs['Controller(' + gameRoom.controller.pos.x + ',' + gameRoom.controller.pos.y + ')'] = {'JobObjId': gameRoom.controller.id, 'JobObjCreeps': []};
                     // FillSpawnExtension
                     RefreshFillSpawnExtensionJobs(gameRoom, roomObjJobs);
                     // Construction
                     RefreshConstructionJobs(gameRoom, roomObjJobs);
                     // Repair
                     RefreshRepairJobs(gameRoom, roomObjJobs);
-                    // Source
-                    const sources = gameRoom.find(FIND_SOURCES);
-                    for (const sourceKey in sources) {
-                        const source = sources[sourceKey];
-                        new RoomVisual(gameRoom.name).text("🏭💼", source.pos.x, source.pos.y);
-                        roomObjJobs['Source(' + source.pos.x + ',' + source.pos.y + ')'] = {'JobObjId': source.id, 'JobObjCreeps': []};
+                    if(fullUpdate){
+                        // Controller
+                        new RoomVisual(gameRoom.name).text("💼", gameRoom.controller.pos.x, gameRoom.controller.pos.y);
+                        roomObjJobs['Controller(' + gameRoom.controller.pos.x + ',' + gameRoom.controller.pos.y + ')'] = {'JobObjId': gameRoom.controller.id, 'JobObjCreeps': []};
+                        // Source
+                        const sources = gameRoom.find(FIND_SOURCES);
+                        for (const sourceKey in sources) {
+                            const source = sources[sourceKey];
+                            new RoomVisual(gameRoom.name).text("🏭💼", source.pos.x, source.pos.y);
+                            roomObjJobs['Source(' + source.pos.x + ',' + source.pos.y + ')'] = {'JobObjId': source.id, 'JobObjCreeps': []};
+                        }
+                        roomObjJobs['Idle'] = {'JobObjCreeps': []};
                     }
-                    roomObjJobs['Idle'] = {'JobObjCreeps': []};
                     break;
                 default:
                     console.log("MemRooms, createDefaultJobs: ERROR! level not found");
@@ -112,6 +114,10 @@ const MemRooms = {
                 };
         }
 
+        function RefreshJobsInRoom(gameRoom){
+
+        }
+
         function RefreshFillStorageJobs(gameRoom, roomObjJobs){
             const fillStorages = gameRoom.find(FIND_MY_STRUCTURES, {
                 filter: (s) => {
@@ -125,6 +131,7 @@ const MemRooms = {
                 roomObjJobs['FillStorage' + fillStorage.structureType.substring(10) + '(' + fillStorage.pos.x + ',' + fillStorage.pos.y + ')'] = {'JobObjId': fillStorage.id, 'JobObjCreeps': []};
             }
         }
+
         function RefreshFillTowerJobs(gameRoom, roomObjJobs){
             const fillTowers = gameRoom.find(FIND_MY_STRUCTURES, {
                 filter: (s) => {
@@ -137,6 +144,7 @@ const MemRooms = {
                 roomObjJobs['FillTower(' + fillTower.pos.x + ',' + fillTower.pos.y + ')'] = {'JobObjId': fillTower.id, 'JobObjCreeps': []};
             }
         }
+
         function RefreshFillSpawnExtensionJobs(gameRoom, roomObjJobs){
             const fillSpawnExtensions = gameRoom.find(FIND_MY_STRUCTURES, {
                 filter: (s) => {
@@ -149,6 +157,7 @@ const MemRooms = {
                 roomObjJobs['FillSpawnExtension(' + fillSpawnExtension.pos.x + ',' + fillSpawnExtension.pos.y + ')'] = {'JobObjId': fillSpawnExtension.id, 'JobObjCreeps': []};
             }
         }
+
         function RefreshResourceDropJobs(gameRoom, roomObjJobs){
             const resourceDrops = gameRoom.find(FIND_DROPPED_RESOURCES, {filter: (drop) => {return (drop.amount > 50);}});
             for (const resourceDropKey in resourceDrops) {
@@ -157,6 +166,7 @@ const MemRooms = {
                 roomObjJobs['ResourceDrop' + resourceDrop.resourceType.substring(9) + '(' + resourceDrop.pos.x + ',' + resourceDrop.pos.y + ',' + resourceDrop.amount + ')'] = {'JobObjId': resourceDrop.id, 'JobObjCreeps': []};
             }
         }
+
         function RefreshConstructionJobs(gameRoom, roomObjJobs){
             const constructions = gameRoom.find(FIND_CONSTRUCTION_SITES);
             for (const constructionKey in constructions) {
@@ -165,6 +175,7 @@ const MemRooms = {
                 roomObjJobs['Construction' + construction.structureType.substring(10) + '(' + construction.pos.x + ',' + construction.pos.y + ')'] = {'JobObjId': construction.id, 'JobObjCreeps': []};
             }
         }
+
         function RefreshRepairJobs(gameRoom, roomObjJobs){
             const repairs = gameRoom.find(FIND_STRUCTURES, {
                 filter: (s) => {
