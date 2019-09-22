7cc11c21992d8cc9fe295941b019f68574d025b7
Frederik <hulmir@gmail.com>
2019-09-22 23:59:20 +0200

    small update

--- main.js
+++ main.js
@@ -55,11 +55,6 @@ module.exports.loop = function () {
 // cache paths to be reused by creeps
 // recycle creeps if there are many idle!
 
-// TODO transporters should look at a job and set it to done if it looks complete - need to do this because of job persistence
-
-
-// TODO rewrite MaxCreeps system to instead of using counting then using creep names to monitor if a creep dies it gets "removed" from the right MaxCreeps-room if the creep has moved to another room
-// TODO add code to rescue stranded idle creeps from other rooms - is done but could be rewritten if i rewrite MaxCreeps
-
 // TODO cleanup remove jobImportance on jobs - controller1,2 jobs should be moved further down
 // TODO problem with how jobs now are preserved - priority does not shift this means that controller1,2 jobs has a larger priority than they should
+
