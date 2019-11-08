f7149ea83a056cc2d7fdd45fef1f6c809ee4cc23
Frederik Brokøb Sørensen <hulmir@gmail.com>
2019-11-08 15:17:55 +0100

    Add files via upload

--- PowerCreeps.js
+++ PowerCreeps.js
@@ -3,52 +3,52 @@ const PowerCreeps = {
         for (const powerCreepKey in Game.powerCreeps) {
             const powerCreep = Game.powerCreeps[powerCreepKey];
             let result = OK;
-            if(powerCreep.spawnCooldownTime && !(powerCreep.spawnCooldownTime > Date.now())) {
+            if(!powerCreep.pos && !powerCreep.spawnCooldownTime) {
                 powerCreep.spawn(Game.getObjectById('5daecf40eb466e8543e2f82d'));
             }else if(!powerCreep.spawnCooldownTime){
-                if(powerCreep.ticksToLive < 500){
+                if(powerCreep.ticksToLive < 500){ // power creep needs to be renewed
                     result = RenewPowerCreep(powerCreep);
                     console.log('PowerCreeps trying to renew ' + powerCreep.name + ' ' + powerCreep.memory.JobName + ' ' + result + ' ticksToLive ' + powerCreep.ticksToLive);
-                }else if(powerCreep.className === POWER_CLASS.OPERATOR){
-                    let target;
-                    powerCreep.memory.JobName = 'GenOps'; // all POWER_CLASS.OPERATOR should just PWR_GENERATE_OPS right now
-                    if(powerCreep.memory.JobName === 'GenOps' && powerCreep.powers[PWR_GENERATE_OPS].cooldown === 0){ // PWR_GENERATE_OPS
-                        if(powerCreep.store.getUsedCapacity() !== 0 || powerCreep.memory.SecondaryJobName === 'depositInventory'){
-                            powerCreep.memory.SecondaryJobName = 'depositInventory';
-                            for (const resourceType in powerCreep.store) {
-                                if (powerCreep.store[resourceType] > 0) {
-                                    result = powerCreep.transfer(powerCreep.room.storage, resourceType);
-                                    if(result === OK && powerCreep.store[resourceType] !== powerCreep.store.getUsedCapacity()){
-                                        result = ERR_BUSY;
-                                    }
-                                    break;
-                                }
-                            }
-                        }else if(!powerCreep.memory.SecondaryJobName){
-                            result = powerCreep.usePower(PWR_GENERATE_OPS);
-                        }else if(powerCreep.memory.SecondaryJobName === 'enableRoom'){
-                            target = powerCreep.room.controller;
-                            result = powerCreep.enableRoom(target);
-                        }
-
-                        if(result === ERR_INVALID_ARGS){ // Using powers is not enabled on the Room Controller.
-                            powerCreep.memory.SecondaryJobName = 'enableRoom';
-                        }else if(powerCreep.memory.SecondaryJobName === 'enableRoom' && result === ERR_NOT_IN_RANGE){
-                            result = powerCreep.moveTo(target);
-                        }else if(powerCreep.memory.SecondaryJobName === 'depositInventory' && result === ERR_NOT_IN_RANGE){
-                            result = powerCreep.moveTo(powerCreep.room.storage);
-                        }else if((powerCreep.memory.SecondaryJobName === 'enableRoom' || powerCreep.memory.SecondaryJobName === 'depositInventory') && result === OK){
-                            powerCreep.memory.SecondaryJobName = undefined;
-                        }
-                        console.log('PowerCreeps ' + powerCreep.name + ' ' + powerCreep.memory.JobName + ' ' + result);
-                    }
+                }else if(powerCreep.room.controller && powerCreep.room.controller.my && !powerCreep.room.controller.isPowerEnabled){ // my room is not power enabled
+                    result = EnablePowerInRoom(powerCreep);
+                    console.log('PowerCreeps trying to EnablePowerInRoom ' + powerCreep.name + ' ' + powerCreep.pos.roomName);
+                }else if(powerCreep.className === POWER_CLASS.OPERATOR){ // power creep is not too old and power is enabled in the room
+                    result = GenerateOps(powerCreep);
                 }
             }else{
                 console.log('PowerCreep ' + powerCreep.name + ' hours left ' + ((((powerCreep.spawnCooldownTime - Date.now()) / 1000) / 60) / 60));
             }
         }
 
-        // TODO only looks for it in the current room
+        function GenerateOps(powerCreep){
+            let result;
+            if(powerCreep.powers[PWR_GENERATE_OPS].cooldown === 0){
+                result = powerCreep.usePower(PWR_GENERATE_OPS);
+                console.log('PowerCreeps ' + powerCreep.name + ' generating OPS ' + powerCreep.name + ' ' + powerCreep.memory.JobName + ' ' + result + ' in ' + powerCreep.pos.roomName);
+            }
+            if(powerCreep.store.getUsedCapacity() > 0){
+                for (const resourceType in powerCreep.store) {
+                    if (powerCreep.store[resourceType] > 0) {
+                        result = powerCreep.transfer(powerCreep.room.storage, resourceType);
+                        break;
+                    }
+                }
+            }
+            if(result === ERR_NOT_IN_RANGE){
+                result = powerCreep.moveTo(powerCreep.room.storage);
+            }
+            return result;
+        }
+
+        function EnablePowerInRoom(powerCreep){
+            let result = powerCreep.enableRoom(powerCreep.room.controller);
+            if(result === ERR_NOT_IN_RANGE){
+                result = powerCreep.moveTo(powerCreep.room.controller);
+            }
+            return result;
+        }
+
+        // TODO only looks for renew sources in the current room
         /**@return {number}*/
         function RenewPowerCreep(powerCreep){
             let powerSpawnOrBank;
@@ -65,6 +65,8 @@ const PowerCreeps = {
                 result = powerCreep.renew(powerSpawnOrBank);
                 if(result === ERR_NOT_IN_RANGE){
                     result = powerCreep.moveTo(powerSpawnOrBank);
+                }else if(result === OK){
+                    powerCreep.memory.PowerSpawnOrBankId = undefined;
                 }
             }
             return result;
