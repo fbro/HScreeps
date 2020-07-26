785c04a1beec366ef0e5b18b3577b18836d90df8
Frederik <hulmir@gmail.com>
2020-07-26 20:26:26 +0200

    small changes

--- Factories.js
+++ Factories.js
@@ -77,7 +77,7 @@ const Factories = {
                 const result = factory.produce(resToProduceName);
                 /**/
                 Util.Info('Factories', 'Produce',
-                    factory.pos.roomName + ' producing ' + resToProduceName + ' ' + factory.store.getUsedCapacity(resToProduceName) + ' result ' + result
+                    factory.level + ' ' + factory.pos.roomName + ' producing ' + resToProduceName + ' ' + factory.store.getUsedCapacity(resToProduceName) + ' result ' + result
                     + ' ' + res1Name + ' ' + factory.store.getUsedCapacity(res1Name)
                     + ' ' + res2Name + ' ' + factory.store.getUsedCapacity(res2Name)
                     + (res3Name ? ' ' + res3Name + ' ' + factory.store.getUsedCapacity(res3Name) : '')
